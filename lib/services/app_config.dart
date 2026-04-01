b9142a6d8afc3638bb7af3803809a4ba7b108e82
kalonjibenjamin <78647782+kalonjibenjamin@users.noreply.github.com>
2026-04-01 21:03:22 +0100

    Update app_config.dart

--- lib/services/app_config.dart
+++ lib/services/app_config.dart
@@ -1,11 +1,4 @@
 class AppConfig {
-  static const String baseUrl = String.fromEnvironment(
-    'DUMIYA_BASE_URL',
-    defaultValue: 'https://microfinance-rdc.online',
-  );
-
-  static const String db = String.fromEnvironment(
-    'DUMIYA_DB',
-    defaultValue: 'finance',
-  );
+  static const String baseUrl = 'https://microfinance-rdc.online';
+  static const String db = 'finance';
 }
